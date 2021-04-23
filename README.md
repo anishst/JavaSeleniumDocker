ac46b44acbe632091f2856ceb77eef1bd2dfbc4a
Anish Sebastian <anishst@hotmail.com>
2021-04-23 17:09:12 -0400

    added code to work the selenium grid

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ Java Maven Project to test Selenium Framework with
 
 1. right click on ```testng.xml``` file and run to saucedemo test
 1. right click on ```search_module.xml``` file and run to duck duck go  tests
+3. from command  line: ```mvn clean test -DsuiteXmlFile=duck_search_tests.xml``` (NOT WORKING Due to parameters)
 
+## Troubleshooting
+
+Error: Parameter 'keyword' is required by @Test on method search but has not been marked @Optional or defined
+https://testng.org/doc/documentation-main.html#parameters
 
 ## examples
 - https://github.com/vinsguru/selenium-docker
--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -2,16 +2,40 @@ package com.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public  void setupDriver() {
-        this.driver = new ChromeDriver();
+    public  void setupDriver() throws MalformedURLException {
+
+//        default values
+//        String host = "localhost"; // use this if grid is on same machine as test
+        String host = "192.168.1.25";  // ip of your selenium grid server if running on diff machine
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+
+//
+        if(System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc = DesiredCapabilities.firefox();
+        }else{
+            dc = DesiredCapabilities.chrome();
+        }
+
+        if(System.getProperty("HUB_HOST") != null){
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
     }
 
     @AfterTest
